e378e165bd1d1bf4180ec79cc9cbc362bcf418c0
jarra <suhrawardi@gmail.com>
2010-10-09 18:09:34 +0200

    Specs improved for the inbox.

--- lib/capybara_email/inbox.rb
+++ lib/capybara_email/inbox.rb
@@ -4,39 +4,49 @@ module CapybaraEmail
   module Inbox
     include Deliveries
 
+    def empty_inbox!
+      reset_mailer
+    end
+
     def inbox
-      all_emails
+      all_emails.reverse
     end
 
     def open_email(selector=nil, value=nil)
       if selector.nil?
         last_email
       else
-        f = :"open_email_#{selector.gsub(' ', '_')}"
+        f = :"open_email_#{selector.to_s.gsub(' ', '_')}"
         send(f, value)
       end
     end
 
     def last_email
-      inbox.last || raise('No email has been send yet!')
+      all_emails.last || raise('No email has been send yet!')
     end
 
     def open_email_to(to)
       inbox.find do |m|
         m.to =~ /#{to}/
-      end.first
+      end
+    rescue
+      raise('Nothing found')
     end
 
     def open_email_from(from)
       inbox.find do |m|
         m.from =~ /#{from}/
-      end.first
+      end
+    rescue
+      raise('Nothing found')
     end
 
     def open_email_with_subject(subject)
       inbox.find do |m|
         m.subject =~ /#{subject}/
-      end.first
+      end
+    rescue
+      raise('Nothing found')
     end
 
     def to_nr(nr)
--- spec/capybara_email/inbox_spec.rb
+++ spec/capybara_email/inbox_spec.rb
@@ -4,21 +4,61 @@ describe CapybaraEmail::Inbox do
   include CapybaraEmail::Inbox
 
   before do
-    reset_mailer
+    empty_inbox!
     ActionMailer::Base.perform_deliveries = true
     send_email
   end
 
-  describe 'using cache_mailer' do
+  describe 'the size of the inbox' do
   
     it 'should have 1 email in the inbox' do
       inbox.size.should == 1
     end
 
     it 'should have 2 emails if I send another one' do
-      send_email(:to => 'joe@example.host', :subject => 'Second email')
+      send_email(:to => 'joe@test.host', :subject => 'Second email')
       inbox.size.should == 2
     end
+  
+    it 'should be empty after clearing it' do
+      empty_inbox!
+      inbox.should be_empty
+    end
+  end
 
+  describe 'finding an email' do
+    before do
+      send_email(:to => 'john@test.host', :subject => 'Just an email')
+      send_email(:to => 'rob@test.host', :subject => 'Just an email')
+      send_email(:to => 'jenny@test.host', :subject => 'Third email')
+    end
+    
+    it 'should find the last email' do
+      send_email(:to => 'joe@test.host', :subject => 'Fourth email')
+      open_email.subject.should == 'Fourth email'
+    end
+
+    it 'should raise a decent error on an empty inbox' do
+      empty_inbox!
+      expected_error = 'No email has been send yet!'
+      lambda{ open_email }.should raise_error expected_error
+    end
+
+    it 'should find an email with a given subject' do
+      open_email(:with_subject, 'Just').to.should include('rob@test.host')
+    end
+
+    it 'should find an email for a given addressee' do
+      open_email(:to, 'joe@test.host').subject.should == 'Fourth email'
+    end
+  end
+
+  describe 'finding an email by sender' do
+  end
+
+  describe 'finding an email by receiver' do
+  end
+
+  describe 'finding an email by subject' do
   end
 end
